a2674674fb0df46c56b2c8ccb21c0aea82873fcd
DESKTOP-N41N49P\HP <ravini.dimanthi@webxpay.com>
2024-01-29 22:49:05 +0530

    SIS updates 20240129 Grade and Exam modules

---
+++ src/main/java/Controller/ExamController.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import Model.Exam;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author HP
+ */
+public class ExamController {
+    
+    public int createExam(List<String> exms) throws SQLException{
+        Exam e = new Exam();
+        e.setExam_name(exms.get(0));
+        e.setExam_code(exms.get(1));        
+        e.setYear(exms.get(2));
+        e.setSemester(exms.get(3));
+        e.setFrom_date(exms.get(4));
+        e.setTo_date(exms.get(5));
+        e.setDetails(exms.get(6));
+        
+        int r = e.addExam();
+        
+        return r;
+    }
+    
+}
---
+++ src/main/java/UserLibraries/GetTimes.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UserLibraries;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.HashMap;
+import java.util.Map;
+import static java.util.Map.entry;
+
+/**
+ *
+ * @author HP
+ */
+public class GetTimes {
+    
+    private static LocalDateTime current_dt = LocalDateTime.now();
+    
+    private static Map<String,String> min = Map.ofEntries(
+            entry("January","01"),
+            entry("February","02"),
+            entry("March","03"),
+            entry("April","04"),
+            entry("May","05"),
+            entry("June","06"),
+            entry("July","07"),
+            entry("August","08"),
+            entry("September","09"),
+            entry("October","10"),
+            entry("November","11"),
+            entry("December","12")
+    );
+    
+    private static Map<String,String> mit = Map.ofEntries(
+            entry("01","January"),
+            entry("02","February"),
+            entry("03","March"),
+            entry("04","April"),
+            entry("05","May"),
+            entry("06","June"),
+            entry("07","July"),
+            entry("08","August"),
+            entry("09","September"),
+            entry("10","October"),
+            entry("11","November"),
+            entry("12","December")
+    );
+    
+    public static String getMonthNumber(String month){
+        return min.get(month);
+    }
+    
+    public static String getMonthText(String month){
+        return mit.get(month);
+    }
+    
+    public static String getCurrentYear(){
+        return String.valueOf(current_dt.getYear());
+    }
+    
+    public static String getCurrentMonth(){
+        //return current_dt.getMonth();
+        String mnth = String.valueOf(current_dt.getMonthValue());
+        mnth = mnth.length()==1?("0"+mnth):mnth;
+        String mt = getMonthText(mnth);
+        return mnth;
+    }
+    
+    public static String getCurrentDay(){
+        LocalDateTime current_dt = LocalDateTime.of(2024, 01, 05, 10, 10);
+        return (String.valueOf(current_dt.getDayOfMonth()).length()==2?(String.valueOf(current_dt.getDayOfMonth())):("0"+(current_dt.getDayOfMonth())));        
+    }
+    
+}
